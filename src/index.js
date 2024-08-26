bb303a46d5694c3349eefe67d2f1b1f656a1b9ae
sibyl2116 <sibyl2116@qq.com>
2024-08-26 17:09:26 +0800

    Update user settings

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.42783670.xyz": dockerHub,
+  "quay.42783670.xyz": "https://quay.io",
+  "gcr.42783670.xyz": "https://gcr.io",
+  "k8s-gcr.42783670.xyz": "https://k8s.gcr.io",
+  "k8s.42783670.xyz": "https://registry.k8s.io",
+  "ghcr.42783670.xyz": "https://ghcr.io",
+  "cloudsmith.42783670.xyz": "https://docker.cloudsmith.io",
+  "ecr.42783670.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.42783670.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
